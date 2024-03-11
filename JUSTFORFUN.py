3ebd7e816ea7ec6b7bcc0ef04f0f585a9c708f7e
oscarahe <osaland@hotmail.com>
2024-03-10 18:12:15 -0600

    Add Jupyter Notebook

---
+++ JUSTFORFUN.py
@@ -0,0 +1,34 @@
+ #Make an empty list for storing aliens.
+aliens = []
+
+# Make 30 green aliens.
+for alien_number in range(30):
+      new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+      aliens.append(new_alien)
+
+# Show the first 5 aliens.
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# Show how many aliens have been created.
+print(f"Total number of aliens: {len(aliens)}")
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# Store information about a pizza being ordered.
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+
+# Summarize the order.
+print(f"You ordered a {pizza['crust']}-crust pizza "
+    "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
