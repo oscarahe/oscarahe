2963827bf438e6e6d25cf70904ea0ccd52422061
oscarahe <osaland@hotmail.com>
2024-03-10 18:37:36 -0600

    Add Jupyter Notebook

--- JUSTFORFUN.py
+++ JUSTFORFUN.py
@@ -32,3 +32,16 @@ print(f"You ordered a {pizza['crust']}-crust pizza "
 
 for topping in pizza['toppings']:
     print(f"\t{topping}")
+
+
+favorite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c'],
+    'edward': ['rust', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+for language in languages:
+        print(f"\t{language.title()}")
