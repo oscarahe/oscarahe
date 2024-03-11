c1dce2bb063625290877d0d0a713350c2c1342a1
oscarahe <osaland@hotmail.com>
2024-03-10 19:26:28 -0600

    Add Jupyter Notebook

--- JUSTFORFUN.py
+++ JUSTFORFUN.py
@@ -49,3 +49,14 @@ for name, languages in favorite_languages.items():
     print(f"\n{name.title()}'s favorite languages are:")
     for language in languages:
         print(f"\t{language.title()}")
+
+#This example shows one way to build a multiline string. 
+#The first line assigns the first part of the message to the variable prompt.
+#In the second line, the operator += takes the string that was assigned to prompt and adds the new string onto the end.
+#The prompt now spans two lines, again with space after the question mark for clarity:
+
+prompt = "If you share your name, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
